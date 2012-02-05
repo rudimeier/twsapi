375ca76688dd5e666dc4dd9f03d53b60eee9d2ce
Ruediger Meier <ruediger.meier@ga-group.nl>
2012-02-05 10:22:02 +0100

    style, make set_socket_nonblock() more readable
    - using fcntl's return values more reliable now.
    - delare static inline

--- PosixSocketClient/src/EPosixClientSocketPlatform.h
+++ PosixSocketClient/src/EPosixClientSocketPlatform.h
@@ -35,15 +35,6 @@
 	};
 	inline bool SocketsDestroy() { return ( !WSACleanup()); };
 	inline int SocketClose(int sockfd) { return closesocket( sockfd); };
-
-	inline int set_socket_nonblock(int sockfd)
-		{
-			unsigned long mode = 1;
-			if( ioctlsocket(sockfd, FIONBIO, &mode) == NO_ERROR ) {
-				return 0;
-			}
-			return -1;
-		}
 	}
 
 #else
@@ -61,24 +52,33 @@
 	inline bool SocketsInit() { return true; };
 	inline bool SocketsDestroy() { return true; };
 	inline int SocketClose(int sockfd) { return close( sockfd); };
-
-	inline int set_socket_nonblock(int sockfd)
-	{
-		int flags = fcntl( sockfd, F_GETFL, 0 );
-		if( flags < 0 ) {
-			return -1;
-		}
-		if( fcntl(sockfd, F_SETFL, flags | O_NONBLOCK)  < 0 ) {
-			return -1;
-		}
-		return 0;
-	}
 	}
 
 #endif
 
 
 namespace IB {
+
+
+static inline int
+set_socket_nonblock(int sockfd)
+{
+#if defined _WIN32
+	unsigned long mode = 1;
+	if( ioctlsocket(sockfd, FIONBIO, &mode) != NO_ERROR ) {
+		return -1;
+	}
+#else
+	int flags = fcntl( sockfd, F_GETFL, 0 );
+	if( flags == -1 ) {
+		return -1;
+	}
+	if( fcntl(sockfd, F_SETFL, flags | O_NONBLOCK)  == -1 ) {
+		return -1;
+	}
+#endif
+	return 0;
+}
 
 
 static inline int
