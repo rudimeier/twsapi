e1e37233ac28e127d7e060028bef373f66b23760
Ruediger Meier <ruediger.meier@ga-group.nl>
2013-02-07 00:31:12 +0100

    build fix, include sys/socket.h (fix OpenBSD)

--- PosixSocketClient/src/EPosixClientSocketPlatform.h
+++ PosixSocketClient/src/EPosixClientSocketPlatform.h
@@ -46,6 +46,12 @@
 	#include <netdb.h>
 	#include <fcntl.h>
 	#include <unistd.h>
+	#ifdef HAVE_SYS_TYPES_H
+		#include <sys/types.h>
+	#endif
+	#ifdef HAVE_SYS_SOCKET_H
+		#include <sys/socket.h>
+	#endif
 
 	namespace IB {
 	// helpers
