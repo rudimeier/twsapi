caa0f240ae7a509173eff4f17f42bd39fab4242a
Ruediger Meier <ruediger.meier@ga-group.nl>
2018-02-19 16:47:21 +0100

    for-upstream: consolidate all copyright headers
     - add missing copyright headers
     - remove annoying UTF8 marker FEFF from first lines
     - fix missing  newlines at the and of files

--- cpp/TestCppClient/ContractSamples.h
+++ cpp/TestCppClient/ContractSamples.h
@@ -1,5 +1,6 @@
-/* Copyright (C) 2013 Interactive Brokers LLC. All rights reserved.  This code is subject to the terms
+/* Copyright (C) 2013 Interactive Brokers LLC. All rights reserved. This code is subject to the terms
  * and conditions of the IB API Non-Commercial License or the IB API Commercial License, as applicable. */
+
 #pragma once
 #ifndef CONTRACTSAMPLE
 #define CONTRACTSAMPLE
--- cpp/client/OperatorCondition.h
+++ cpp/client/OperatorCondition.h
@@ -1,3 +1,6 @@
+/* Copyright (C) 2018 Interactive Brokers LLC. All rights reserved. This code is subject to the terms
+ * and conditions of the IB API Non-Commercial License or the IB API Commercial License, as applicable. */
+
 #pragma once
 #include "OrderCondition.h"
 
